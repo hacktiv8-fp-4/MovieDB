1d2e47e2792fb901aa4189cc0ddd36fbc7bbddcc
unknown <bilalalghiffari53@gmail.com>
2023-06-09 17:42:03 +0700

    tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 import { useGetAllMovieQuery } from "./redux/slice/slice-movie";
 
 function App() {
   const { data } = useGetAllMovieQuery(undefined);
-  // console.log("data", );
+  console.log("data", data);
   return (
     <div>
-      <h3>Hello</h3>
+      <h3 className="text-green-400">Hello</h3>
     </div>
   );
 }
