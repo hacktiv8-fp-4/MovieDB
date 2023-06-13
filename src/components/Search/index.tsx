6d32ace42350adeebb11660231ead28c6ac6652c
ibal.libul <ibal.libul>
2023-06-13 13:48:45 +0700

    add search

--- src/components/Search/index.tsx
+++ src/components/Search/index.tsx
@@ -3,7 +3,8 @@ const Search = () => {
     <div>
       <label
         htmlFor="hs-trailing-button-add-on-with-icon-and-button"
-        className="sr-only">
+        className="sr-only"
+      >
         Label
       </label>
       <div className="relative flex rounded-md">
@@ -21,21 +22,24 @@ const Search = () => {
             width="16"
             height="16"
             fill="currentColor"
-            viewBox="0 0 16 16">
+            viewBox="0 0 16 16"
+          >
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
           </svg>
         </div>
         {/* Search Button */}
         <button
           type="button"
-          className="py-3 px-4 bg-indigo-500 hover:bg-indigo-600 transition-all inline-flex flex-shrink-0 justify-center items-center rounded-r-md border border-transparent font-semibold text-white focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm">
+          className="py-3 px-4 bg-indigo-500 hover:bg-indigo-600 transition-all inline-flex flex-shrink-0 justify-center items-center rounded-r-md border border-transparent font-semibold text-white focus:z-10 focus:outline-none focus:ring-2 focus:ring-blue-500 text-sm"
+        >
           <svg
             className="h-4 w-4 "
             xmlns="http://www.w3.org/2000/svg"
             width="16"
             height="16"
             fill="currentColor"
-            viewBox="0 0 16 16">
+            viewBox="0 0 16 16"
+          >
             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
           </svg>
         </button>
